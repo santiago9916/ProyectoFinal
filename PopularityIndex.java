5729772975494c109f7446820292ffbd2fc56f0b
santiago9916 <santiago.malaverp@uniagustiniana.edu.co>
2026-05-10 00:05:04 -0500

    Update PopularityIndex.java

--- PopularityIndex.java
+++ PopularityIndex.java
@@ -1,6 +1,70 @@
+/*
+almacena la popularidad de una publicacion
+*/
+package com.mycompany.proyectofinal3;
 
-package com.mycompany.proyectfinal;
+public class NodoAVL {
+    int popularidad;
+    int publicacionId;
+    int altura;
+    NodoAVL izquierdo;
+    NodoAVL derecho;
 
-public class PopularityIndex {
+    public NodoAVL(int publicacionId, int popularidad) {
+        this.publicacionId = publicacionId;
+        this.popularidad = popularidad;
+        this.altura = 1;
+    }
+
+    public NodoAVL(int popularidad, int publicacionId, int altura, NodoAVL izquierdo, NodoAVL derecho) {
+        this.popularidad = popularidad;
+        this.publicacionId = publicacionId;
+        this.altura = 1;
+        this.izquierdo = izquierdo;
+        this.derecho = derecho;
+    }
+
+    public int getPopularidad() {
+        return popularidad;
+    }
+
+    public void setPopularidad(int popularidad) {
+        this.popularidad = popularidad;
+    }
+
+    public int getPublicacionId() {
+        return publicacionId;
+    }
+
+    public void setPublicacionId(int publicacionId) {
+        this.publicacionId = publicacionId;
+    }
+
+    public int getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public NodoAVL getIzquierdo() {
+        return izquierdo;
+    }
+
+    public void setIzquierdo(NodoAVL izquierdo) {
+        this.izquierdo = izquierdo;
+    }
+
+    public NodoAVL getDerecho() {
+        return derecho;
+    }
+
+    public void setDerecho(NodoAVL derecho) {
+        this.derecho = derecho;
+    }
+    
     
 }
+
+

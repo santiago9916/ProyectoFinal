d2d4a7dd831257eb7650b7441799bd49e22c5f8c
santiago9916 <santiago.malaverp@uniagustiniana.edu.co>
2026-05-10 00:02:59 -0500

    Update NodoArbol.java

--- NodoArbol.java
+++ NodoArbol.java
@@ -1,6 +1,39 @@
+/*
+nodo del arbol de publicaciones que puede contener una publicacion o comentario
+*/
+package com.mycompany.proyectofinal3;
 
-package com.mycompany.proyectfinal;
+import java.util.List;
 
 public class NodoArbol {
+    public object data;
+    public List<NodoArbol> hijos;
+
+    public NodoArbol(object data, List hijos) {
+        this.data = data;
+        this.hijos = hijos;
+    }
+
+    public object getData() {
+        return data;
+    }
+
+    public void setData(object data) {
+        this.data = data;
+    }
+
+    public List getHijos() {
+        return hijos;
+    }
+
+    public void setHijos(List hijos) {
+        this.hijos = hijos;
+    }
+    
+    public void agregarhijo (NodoArbol hijo){
+        this.hijos.add(hijo);
+    }
+    
+    
     
 }

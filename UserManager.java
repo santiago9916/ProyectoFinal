86c3f2bb496f2221621f3d138693db43ddaa7d8b
santiago9916 <santiago.malaverp@uniagustiniana.edu.co>
2026-05-11 21:31:10 -0500

    Update UserManager.java

--- UserManager.java
+++ UserManager.java
@@ -2,7 +2,15 @@
 gestiona el registro y acceso de los usuarios
 */
 package com.mycompany.proyectfinal;
+/*
+importacion
+*/
 import java.util.HashMap;
+
+/*
+ crea el HashMap:en Java sirve para guardar datos usando una clave y un valor.
+#Funciona como una especie de diccionario o tabla
+*/
 public class UserManager {
     
     public HashMap<String, Usuario> usuario;
@@ -11,14 +19,18 @@ public class UserManager {
 
         usuario = new HashMap<>();
     }
-
+/*
+    con el f verifica si existe el username
+    */
     public void registrarUsuarios(String username){
         if (usuario.containsKey(username)){
             System.out.println("El usuario ya existe :( ");
   } else {
 
             Usuario nuevousuario = new Usuario(username);
-
+/*
+            el (usuario.put guarda el usuario en el HashMap)
+            */
             usuario.put(username, nuevousuario);
 
             System.out.println("El usuario fue registrado correctamente :)");

28e2a825e861a845d84e70af083d2fb9b74e84e8
santiago9916 <santiago.malaverp@uniagustiniana.edu.co>
2026-05-11 21:58:40 -0500

    Update NodoAVL.java

--- NodoAVL.java
+++ NodoAVL.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
 
-package com.mycompany.proyectfinal;
+public class NodoArbol {
+    private Object data;
+    private List<NodoArbol> hijos;
 
-public class NodoAVL {
-    
+    public NodoArbol(Object data) {
+        this.data = data;
+        this.hijos = new ArrayList<>();
+    }
+
+    public void addHijo(NodoArbol hijo) {
+        hijos.add(hijo);
+    }
+
+    public Object getData() { return data; }
+    public List<NodoArbol> getHijos() { return hijos; }
+
+    public void mostrar(String prefijo) {
+        System.out.println(prefijo + data.toString());
+        for (NodoArbol hijo : hijos) {
+            hijo.mostrar(prefijo + "   ");
+        }
+    }
 }

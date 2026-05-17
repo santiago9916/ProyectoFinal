dc6b2fc4ea5b11f68931fa83f6ea5b2ba1ceb9ad
santiago9916 <santiago.malaverp@uniagustiniana.edu.co>
2026-05-17 18:59:52 -0500

    Update UserManager.java

--- UserManager.java
+++ UserManager.java
@@ -2,45 +2,56 @@
 gestiona el registro y acceso de los usuarios
 */
 package com.mycompany.proyectfinal;
+
 /*
 importacion
 */
 import java.util.HashMap;
 
 /*
- crea el HashMap:en Java sirve para guardar datos usando una clave y un valor.
-#Funciona como una especie de diccionario o tabla
+HashMap:
+Sirve para guardar datos usando una clave y un valor.
+Funciona como un diccionario.
 */
 public class UserManager {
-    
-    public HashMap<String, Usuario> usuario;
 
+    // HashMap de usuarios
+    private HashMap<String, Usuario> usuarios;
+
+    // Constructor
     public UserManager() {
 
-        usuario = new HashMap<>();
+        usuarios = new HashMap<>();
     }
-/*
-    con el f verifica si existe el username
+
+    /*
+    Verifica si el usuario ya existe
     */
-    public void registrarUsuarios(String username){
-        if (usuario.containsKey(username)){
+    public void registrarUsuarios(String username) {
+
+        if (usuarios.containsKey(username)) {
+
             System.out.println("El usuario ya existe :( ");
-  } else {
 
-            Usuario nuevousuario = new Usuario(username);
-/*
-            el (usuario.put guarda el usuario en el HashMap)
+        } else {
+
+            Usuario nuevoUsuario =
+                new Usuario(username);
+
+            /*
+            Guarda el usuario en el HashMap
             */
-            usuario.put(username, nuevousuario);
+            usuarios.put(username, nuevoUsuario);
 
-            System.out.println("El usuario fue registrado correctamente :)");
+            System.out.println(
+                "El usuario fue registrado correctamente :)"
+            );
         }
     }
-        public Usuario obtenerUsuario(String username){
-        return usuario.get(username);
-        }   
-}
 
-/*
-REVISAR!!!!!!!!!!!!!
-*/
+    // Obtener usuario
+    public Usuario obtenerUsuario(String username) {
+
+        return usuarios.get(username);
+    }
+}

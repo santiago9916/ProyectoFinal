621afd7117664132a4fca84a144ade1c3269f73c
Robert Said <robert.foriguan@uniagustiniana.edu.co>
2026-05-19 00:40:29 -0500

    Update NodoArbol.java

--- NodoArbol.java
+++ NodoArbol.java
@@ -1,47 +1,47 @@
 /*
 nodo del arbol de publicaciones que puede contener una publicacion o comentario
-*/
-package com.mycompany.proyectofinal3;
+ */
+package proyectofinaled1;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class NodoArbol {
-    public Object data; // Contenido que puede ser publicacion o comentario /
-    private List<NodoArbol> hijos; /* Lista de hijos */
 
-    public NodoArbol(Object data, List<NodoArbol> hijos) {
+    public Object data; // Contenido que puede ser publicacion o comentario /
+    public List<NodoArbol> hijos;
+
+    /* Lista de hijos */
+
+    public NodoArbol(Object data) {
         this.data = data;
         this.hijos = new ArrayList<>(); // Inicializacion de la lista de hijos /
     }
 
-    NodoArbol(Publicacion p) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
-
     // Getters y setters del atributo data y del atributo hijos
-        //data
+    //data
     public Object getData() {
         return data;
     }
+
     public void setData(Object data) {
         this.data = data;
     }
-        //-------
-    
-        //la lista de hijos
+    //-------
+
+    //la lista de hijos
     public List<NodoArbol> getHijos() {
         return hijos;
     }
+
     public void setHijos(List<NodoArbol> hijos) {
         this.hijos = hijos;
     }
-        //-------
-  
+    //-------
+
     //_________________________________________________________
-    
     //Agregará un hijo al nodo en el que este como comentario
-    public void agregarHijo (NodoArbol hijo){ 
+    public void agregarHijo(NodoArbol hijo) {
         this.hijos.add(hijo);
     }
 
@@ -58,5 +58,5 @@ public class NodoArbol {
             }
         }
         return -1;
-    }  
+    }
 }

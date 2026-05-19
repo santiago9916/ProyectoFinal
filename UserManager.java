888fb7147093b1dbe5d7f134e8c394b0517bd12c
Robert Said <robert.foriguan@uniagustiniana.edu.co>
2026-05-19 00:39:23 -0500

    Update UserManager.java

--- UserManager.java
+++ UserManager.java
@@ -1,18 +1,13 @@
 /*
 gestiona el registro y acceso de los usuarios
-*/
-package com.mycompany.proyectfinal;
+ */
+package proyectofinaled1;
 
 /*
 importacion
-*/
+ */
 import java.util.HashMap;
 
-/*
-HashMap:
-Sirve para guardar datos usando una clave y un valor.
-Funciona como un diccionario.
-*/
 public class UserManager {
 
     // HashMap de usuarios
@@ -20,14 +15,13 @@ public class UserManager {
 
     // Constructor
     public UserManager() {
-
         usuarios = new HashMap<>();
     }
 
     /*
     Verifica si el usuario ya existe
-    */
-    public void registrarUsuarios(String username) {
+     */
+    public void registrarUsuario(String username) {
 
         if (usuarios.containsKey(username)) {
             System.out.println("El usuario ya existe :( ");
@@ -38,18 +32,17 @@ public class UserManager {
 
             /*
             Guarda el usuario en el HashMap
-            */
+             */
             usuarios.put(username, nuevoUsuario);
 
             System.out.println(
-                "El usuario fue registrado correctamente :)"
+                    "El usuario fue registrado correctamente :)"
             );
         }
     }
 
     // Obtener usuario
     public Usuario obtenerUsuario(String username) {
-
         return usuarios.get(username);
     }
 }
